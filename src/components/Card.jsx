66aeb1d67ed5b8debac0dd0e7f7ded648cccccc6
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-06-28 02:04:00 +0300

    h3

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import { AnimationCount } from '@/components';
 export default function Card({ number, sign, title, text }) {
   return (
     <div className="bg-muted p-8 md:p-10 lg:p-12 rounded-2xl">
-      <h3 className="font-medium text-[40px] md:text-5xl">
+      <p className="font-medium text-[40px] md:text-5xl">
         <AnimationCount>{number}</AnimationCount>
         {sign}
-      </h3>
+      </p>
       <h3 className="text-xs md:text-base uppercase font-medium pt-2">{title}</h3>
       <p className="text-base md:text-lg pt-4">{text}</p>
     </div>
